6a035b2872ee5d2cc6f94cfe2066b233d4954153
germanJM <chmin1020@gmail.com>
2022-12-23 10:52:22 +0900

    Create 행렬의 곱셈.kt

---
+++ 행렬의 곱셈.kt
@@ -0,0 +1,16 @@
+class Solution {
+    fun solution(arr1: Array<IntArray>, arr2: Array<IntArray>): Array<IntArray> {
+        val answer = Array(arr1.size){IntArray(arr2[0].size)}
+
+        for(i in arr1.indices){
+            for(j in arr2[0].indices){
+                var res = 0
+                for(k in arr2.indices)
+                    res += (arr1[i][k] * arr2[k][j])
+                answer[i][j] = res
+            }
+        }
+        
+        return answer
+    }
+}
