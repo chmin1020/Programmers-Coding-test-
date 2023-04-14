8ca66292f8bd3946f7c6e4b902fda5f87d0a7565
germanJM <chmin1020@gmail.com>
2023-04-14 20:35:51 +0900

    Create 폰켓몬.java

---
+++ 폰켓몬.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for(int i = 0; i < nums.length; i++)
+            set.add(nums[i]);
+        
+        int answer = nums.length / 2;
+        if(set.size() < nums.length / 2)
+            answer = set.size();
+        
+        return answer;
+    }
+}
